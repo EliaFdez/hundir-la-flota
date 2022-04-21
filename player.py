5d5c6e3d8f189fe3873e782984038c2e3fd05a89
EliaFdez <40912556+EliaFdez@users.noreply.github.com>
2022-04-21 11:42:41 +0200

    Add files via upload
    Comentarios iniciales con indicaciones

--- player.py
+++ player.py
@@ -1,3 +1,8 @@
+"""
+Envía mensajes a un canal (clients/flota/jugador/...).
+Recibe mensajes de un canal (clients/flota/sala/...).
+"""
+
 from paho.mqtt.client import Client
 from tkinter import *
 
--- sala.py
+++ sala.py
@@ -1,4 +1,22 @@
+"""
+Envía mensajes a un canal por jugador (clients/flota/sala/...).
+Recibe mensajes de cada jugador por un canal (clients/flota/jugador/...).
+
+Cada vez que recibe información de un jugador, la procesa y manda los tableros actualizados a todos los jugadores.
+"""
+
 from paho.mqtt.client import Client
 from multiprocessing import Lock
 from tkinter import *
 
+ANCHO = 20
+ALTO = 20
+
+
+class Tablero():
+    def __init__(self):
+        self.ancho = ANCHO
+        self.alto = ALTO
+
+    def __str__(self):
+        return (self.ancho, self.alto)
