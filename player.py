7579e14c2a6b3b20758a7a5b6f039266a4bf0899
Elia Fernández Salazar <eliafesa@gmail.com>
2022-04-23 20:05:59 +0200

    clases añadidas

--- player.py
+++ player.py
@@ -3,6 +3,123 @@ Envía mensajes a un canal (clients/flota/jugador/...).
 Recibe mensajes de un canal (clients/flota/sala/...).
 """
 
+from operator import truediv
 from paho.mqtt.client import Client
+import paho.mqtt.publish as publish
 from tkinter import *
+import random
+import traceback
 
+ANCHO = 5
+ALTO = 5
+BARCOS = 3
+
+
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.board = Board()
+
+    def __str__(self):
+        return self.name
+
+class Board():
+    def __init__(self):
+        self.ancho = ANCHO
+        self.alto = ALTO
+        self.barcos = self.first_board()  # 0 agua, 1 barco
+        self.estado = [[0 for i in range(self.ancho)] for j in range(self.alto)]  # 0 nada, 1 disparado
+    
+    def first_board(self):
+        posinit = sorted(random.sample(range(self.ancho*self.alto), BARCOS))
+        tabinit = [[0 for i in range(self.ancho)] for j in range(self.alto)]
+        for i in posinit:
+            tabinit[i//self.ancho][i%self.ancho] = 1
+        return tabinit
+
+    def barcos_to_string(self):
+        pos=' '
+        for i in range(len(self.barcos)):
+            for j in range(len(self.barcos[i])):
+                if self.barcos[i][j] == 1:
+                    pos += str(i) + ',' + str(j) + ' '
+        return pos
+
+    def __str__(self):
+        mostrar = ''
+        for i in range(len(self.barcos)):
+            for j in self.barcos[i]:
+                mostrar += '|' + str(j)
+            mostrar += '|\n'
+
+        return mostrar
+
+def start_game():
+    finished = False
+    print('JUEGO EMPEZADO')
+    while not finished:
+        pos = input('Donde quieres atacar: ')
+
+def on_connect(mqttc, userdata, flags, rc):
+    try:
+        print("CONNECT:", userdata, flags, rc)
+    except:
+        traceback.print_exc()
+
+def on_message(mqttc, userdata, msg):
+    try:
+        print("MESSAGE:", userdata, msg.topic, msg.qos, msg.payload)
+        waiting = True
+        if ('clients/flota/sala/' in msg.topic) and waiting:
+            waiting = False
+            start_game()
+
+    except:
+        traceback.print_exc()
+
+
+def on_publish(mqttc, userdata, mid):
+    try:
+        print("PUBLISH:", userdata, mid)
+    except:
+        traceback.print_exc()
+
+def on_subscribe(mqttc, userdata, mid, granted_qos):
+    try:
+        print("SUBSCRIBED:", userdata, mid, granted_qos)
+    except:
+        traceback.print_exc()
+
+def on_unsubscribe(mqttc, userdata, mid):
+    try:
+        print("UNSUBSCRIBED:", userdata, mid)
+    except:
+        traceback.print_exc()
+
+def main():
+    name = input('Como te llamas? ')
+    if not name:
+        name = ''.join((random.choice('abcdxyzpqr0123456789') for i in range(5)))
+
+    print(f'Bienvenido {name}')
+    
+    player = Player(name)
+
+    mqttc = Client(userdata=name)
+
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    mqttc.on_unsubscribe = on_unsubscribe
+
+    mqttc.connect("picluster02.mat.ucm.es")
+
+    mqttc.subscribe('clients/flota/sala/' + name)
+    print('clients/flota/sala/' + name)
+    mqttc.publish('clients/flota/jugador', player.name + player.board.barcos_to_string())
+    
+    mqttc.loop_forever()
+
+if __name__ == "__main__":
+    main()
--- sala.py
+++ sala.py
@@ -8,15 +8,126 @@ Cada vez que recibe información de un jugador, la procesa y manda los tableros 
 from paho.mqtt.client import Client
 from multiprocessing import Lock
 from tkinter import *
+import random
+import traceback
 
-ANCHO = 20
-ALTO = 20
+ANCHO = 5
+ALTO = 5
+BARCOS = 3
 
 
-class Tablero():
-    def __init__(self):
+class Player():
+    def __init__(self, name, pos):
+        self.name = name
+        self.board = Board(pos)
+    
+    def __str__(self):
+        return self.name
+
+class Board():
+    def __init__(self, pos):
         self.ancho = ANCHO
         self.alto = ALTO
+        self.barcos = self.build_board(pos) # 0 agua, 1 barco
+        self.status = [[0 for i in range(self.ancho)] for j in range(self.alto)]  # 0 nada, 1 disparado
+    
+    def build_board(self, pos):
+        tab = [[0 for i in range(self.ancho)] for j in range(self.alto)]
+        for i in pos:
+            tab[i[0]][i[1]] = 1
+        return tab
 
-    def __str__(self):
-        return (self.ancho, self.alto)
+    def actualizar(self, pos):
+        self.status[pos[1]][pos[0]] = 1
+
+class Game():
+    def __init__(self, jug1, jug2):
+        self.jugador1 = jug1
+        self.jugador2 = jug2
+    
+    def show_boats(self, jug):
+        mostrar = ''
+        for i in range(len(jug.board.barcos)):
+            for j in jug.board.barcos[i]:
+                mostrar += '|' + str(j)
+            mostrar += '|\n'
+
+        return mostrar
+    
+    def show_status(self, jug):
+        mostrar = ''
+        for i in range(len(jug.status)):
+            for j in jug.status[i]:
+                if j == 1:
+                    mostrar += '|' + str(jug.board.barcos[i][j])
+                else:
+                    mostrar += '|' + ' '
+            mostrar += '|\n'
+
+        return mostrar
+
+    def show_boards(self, jug):
+        jugboard = self.show_boats(jug)
+        if jug == self.jugador1:
+            otherboard = self.show_status(self.jugador2)
+        else:
+            otherboard = self.show_status(self.jugador1)
+            
+        return jugboard + '\n' + otherboard
+
+    def change_board(self, jug, pos):
+        jug.board.status[pos[1]][pos[0]] = 1
+
+def on_connect(mqttc, userdata, flags, rc):
+    try:
+        print("CONNECT:", userdata, flags, rc)
+    except:
+        traceback.print_exc()
+
+def on_message(mqttc, userdata, msg):
+    try:
+        print("MESSAGE:", userdata, msg.topic, msg.qos, msg.payload)
+        if msg.topic == 'clients/flota/jugador':
+            data = msg.payload.split()
+            name = str(data[0])[2:-1]
+            pos = []
+            for i in data[1:]:
+                pos.append((int(str(i)[2]),int(str(i)[4])))
+            player1 = Player(name, pos)
+
+            mqttc.subscribe(msg.topic + name)
+            
+            mqttc.publish('clients/flota/sala/' + name, 'HA JUGAR')
+            print('clients/flota/sala/' + name)
+
+    except:
+        traceback.print_exc()
+
+
+def on_publish(mqttc, userdata, mid):
+    try:
+        print("PUBLISH:", userdata, mid)
+    except:
+        traceback.print_exc()
+
+def on_subscribe(mqttc, userdata, mid, granted_qos):
+    try:
+        print("SUBSCRIBED:", userdata, mid, granted_qos)
+    except:
+        traceback.print_exc()
+
+def main():
+    mqttc = Client(userdata='sala')
+    
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+
+    mqttc.connect("picluster02.mat.ucm.es")
+    mqttc.subscribe('clients/flota/jugador')
+
+    mqttc.loop_forever()
+
+if __name__ == "__main__":
+    main()
